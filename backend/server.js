f89d4947f16428c09a391203e6d9d8e4886bc9c5
Luis Aguilar <luisaguilar159@gmail.com>
2024-04-24 00:02:58 -0700

    Add test page functionality

--- backend/server.js
+++ backend/server.js
@@ -45,6 +45,7 @@ app.use("/lobby", routes.lobby);
 app.use("/games", routes.games);
 app.use("/api", routes.api);
 app.use("/chat", routes.chat);
+app.use("/test", routes.test);
 
 server.listen(PORT, () => {
   console.log(
