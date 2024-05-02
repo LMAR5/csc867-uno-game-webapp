9a0c05c5c63dc79926adad163551603cbf453072
Luis Aguilar <luisaguilar159@gmail.com>
2024-05-01 19:25:29 -0700

    Add dialog to create game #22
    Add dialog element to lobby ejs, js functions to handle dialog to create games in frontend folder

--- frontend/games/index.js
+++ frontend/games/index.js
@@ -1,1 +1,5 @@
+import { triggerDialog as createGameDialog } from './create-game-dialog';
+import { hiddeGameForm as createGameHideDialog } from './create-game-dialog';
+
 console.log("Hello from games module");
+export default [createGameDialog, createGameHideDialog];
